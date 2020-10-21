436b9e5c6265342408035ede18bb889feb681175
crazyjackkong <crazyjackkong@126.com>
2020-10-21 16:21:16 +0800

    Site updated: 2020-10-21 16:21:11

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/about/index.html","644d0394617f80ba9ddde7abf31b4859"]];
+var precacheConfig = [["/about/index.html","312e1ee08f608e118c27e4105b30628f"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
