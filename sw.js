7e56206ec10554b39fefa958f84ed381c717869f
crazyjackkong <crazyjackkong@126.com>
2021-02-05 21:46:32 +0800

    Site updated: 2021-02-05 21:46:27

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/about/index.html","b1bcc76d4027d34019173ffed751c4cc"]];
+var precacheConfig = [];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
