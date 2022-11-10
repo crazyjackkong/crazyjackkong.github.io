1881dac92862a15c733efa4bd0e9de647a1e271c
crazyjackkong <crazyjackkong@126.com>
2022-11-10 15:49:30 +0800

    Site updated: 2022-11-10 15:49:29

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/about/index.html","d82461f7725b8f764233c338bf12fb6a"]];
+var precacheConfig = [["/about/index.html","dfa0da3932a80aac8a992dfd92860338"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
